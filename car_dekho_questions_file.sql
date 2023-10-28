47bd52c604d3c5fc332c80ef80da5f9aadef4e2d
shubhamjangale260409 <137080409+shubhamjangale260409@users.noreply.github.com>
2023-10-28 23:03:12 +0530

    Add files via upload

---
+++ car_dekho_questions_file.sql
@@ -0,0 +1,51 @@
+create database databasename;
+use databasename;
+
+-- 1] Read cars data.
+
+
+-- 2] Total cars: To get a count of total records.
+
+
+-- 3] The manager asked the employee -
+-- How many cars will be available in 2023.
+
+
+-- 4] The manager asked the employee, How many cars is avaialble in 2020, 2021, 2022.
+
+
+-- 5] Clint asked me to print the total of all cars by year.
+-- I don't see all the details.
+
+
+-- 6] Clint asked to car dealer agent, How many diesel cars will there be in 2020?
+
+
+-- 7] Clint requested a car dealer agent, How many petrol cars will there be in 2020?
+
+
+-- 8] The manager told the employee to give a print All the fuel cars
+-- (petrol, diesel and CNG) come by all year.
+
+
+-- 9] Manager said there were more than 100 cars in a given year, which year had more than 100 cars?
+
+
+-- 10] The manager said to the employee All cars count details between 2015 and 2023; we need a complete list.
+
+
+-- 11] The manager said to the employee All cars details between 2015 to 2023 We need complete list.
+
+
+-- 12] Show all cars, selling price who have 1st Owner there km given between 20000 and 60000 and there fuel type is petrol.
+
+
+-- 13] Show Top 10 cars details who have highest selling price.
+
+
+
+
+
+
+
+
